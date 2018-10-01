a4412ff7851b04232a59c260a45177874bb225b3
Mihail Geshoski <mihail@moodle.com>
2018-10-01 09:18:12 +0800

    MDL-62564 core: delete_user() should not remove the user context

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -4182,9 +4182,6 @@ function delete_user(stdClass $user) {
     // Don't trigger update event, as user is being deleted.
     user_update_user($updateuser, false, false);
 
-    // Now do a final accesslib cleanup - removes all role assignments in user context and context itself.
-    context_helper::delete_instance(CONTEXT_USER, $user->id);
-
     // Any plugin that needs to cleanup should register this event.
     // Trigger event.
     $event = \core\event\user_deleted::create(

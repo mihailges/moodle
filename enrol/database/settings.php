65d203b3cf95d36bd36d00fc4305f3399eb8c204
Mihail Geshoski <mihail@moodle.com>
2020-05-05 12:16:23 +0800

    MDL-67845 enrol_database: Remove usages of deprecated enrolment setting

--- enrol/database/settings.php
+++ enrol/database/settings.php
@@ -113,7 +113,8 @@ if ($ADMIN->fulltree) {
 
     require_once($CFG->dirroot.'/enrol/database/settingslib.php');
 
-    $settings->add(new enrol_database_admin_setting_category('enrol_database/defaultcategory', get_string('defaultcategory', 'enrol_database'), get_string('defaultcategory_desc', 'enrol_database')));
+    $settings->add(new admin_settings_coursecat_select('enrol_database/defaultcategory', get_string('defaultcategory', 'enrol_database'),
+        get_string('defaultcategory_desc', 'enrol_database'), 1));
 
     $settings->add(new admin_setting_configtext('enrol_database/templatecourse', get_string('templatecourse', 'enrol_database'), get_string('templatecourse_desc', 'enrol_database'), ''));
 }

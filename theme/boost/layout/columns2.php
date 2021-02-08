a2d188557bfe8ed27fd6e8d76bd1ca8e9531edee
Bas Brands <bas@moodle.com>
2021-02-08 11:35:58 +0100

    MDL-70202 core_navigation: tweaks after rebasing on changed backend

--- theme/boost/layout/columns2.php
+++ theme/boost/layout/columns2.php
@@ -42,7 +42,7 @@ $hasblocks = strpos($blockshtml, 'data-block=') !== false;
 $buildregionmainsettings = !$PAGE->include_region_main_settings_in_header_actions();
 // If the settings menu will be included in the header then don't add it here.
 $regionmainsettingsmenu = $buildregionmainsettings ? $OUTPUT->region_main_settings_menu() : false;
-$secondarymenu = $PAGE->secondarynav->children;
+$secondarymenu = $PAGE->secondarynav;
 $templatecontext = [
     'sitename' => format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID), "escape" => false]),
     'output' => $OUTPUT,

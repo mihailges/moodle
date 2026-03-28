238e42836bf01d41c8f44eb4321de156105d8f29
Mihail Geshoski <mihail@moodle.com>
2026-03-28 23:58:49 +0800

    fixup! MDL-83509 core_ltix: create plugin service handling parameter inclusion

--- public/ltix/classes/local/ltiservice/plugin_parameters_service.php
+++ public/ltix/classes/local/ltiservice/plugin_parameters_service.php
@@ -5,9 +5,21 @@ namespace core_ltix\local\ltiservice;
 use core_ltix\local\lticore\message\context\collection\launch_context;
 use core_ltix\local\lticore\message\context\item\tool_context;
 
+/**
+ * Service responsible for retrieving parameters from registered service plugins and providing them for launch.
+ *
+ * @internal
+ * @package    core_ltix
+ * @copyright  2026 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 final class plugin_parameters_service implements plugin_parameters_service_interface {
 
-
+    /**
+     * Constructor.
+     *
+     * @param service_plugin_registry $pluginregistry registry to fetch plugin implementations.
+     */
     public function __construct(private service_plugin_registry $pluginregistry) {
     }
 

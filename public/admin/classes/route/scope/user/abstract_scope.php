bdf3e69efbf249849ddf796611b32903104258f2
Mihail Geshoski <mihail@moodle.com>
2026-07-17 23:56:21 +0800

    MDL-89089 core: Implement admin scopes

---
+++ public/admin/classes/route/scope/user/abstract_scope.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace core_admin\route\scope\user;
+
+use core_admin\route\scope\abstract_scope as abstract_admin_scope;
+
+/**
+ * The base abstract class for admin user scopes.
+ *
+ * @package    core_admin
+ * @copyright  2026 Mihail Geshoski <mihailgesoski@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+#[\core\router\scope\name_attribute('user')]
+#[\core\router\scope\description_attribute('scope:admin:user', 'core_admin')]
+abstract class abstract_scope extends abstract_admin_scope {
+}
